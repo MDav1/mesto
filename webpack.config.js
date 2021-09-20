154457090f9b8d7e6c07537274edf312d3032490
Emiliya Davidova <davidova.mila@gmail.com>
2021-09-21 00:56:47 +0300

    project 8

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         type: 'asset/resource',
       },
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, {
             loader: 'css-loader',
             options: {
